294195a7e5e36906cad092700e660ca82b30ded1
Simon Orr <simon@simonorrstudio.com>
2012-08-22 00:50:02 -0700

    Version bump to 0.2.0

---
+++ docs/lpconfig_sample.rb
@@ -0,0 +1,22 @@
+class LPConfig
+
+def self.imgFileExtension
+ '.png'
+end
+
+def self.imgHostUrl
+'http://technowizard12.github.com'
+end
+
+def self.title
+'The Weekly Font'
+end
+
+def self.sampleImg
+'/sample'
+end
+
+def self.iconImg
+'/icon'
+end
+end
